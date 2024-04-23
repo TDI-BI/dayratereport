56d4bd150349c140ac65635f904de43008cac5f0
eygwa <153556337+eygwa@users.noreply.github.com>
2024-04-23 17:02:29 -0500

    changed location of query test
    moved my query test destination to the review page because thats where we are going to be pulling our days anyway

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,9 +1,27 @@
+"use client"; // this is a client componnent (?) idk why i just need this to run things
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Page() {
 
+    const [dataResponse, setdataResponse] = useState([]);
+    useEffect(() => {
+      async function getPageData(){
+        const apiUrlEndpoint = 'http://localhost:3000/api/hello'
+        const response = await fetch(apiUrlEndpoint);
+        const res = await response.json();
+        console.log(res.resp);// -> desnt work idk why
+        setdataResponse(res.resp);
+      }
+      getPageData();
+    }, []);
+
     return (
         <main className="flex min-h-screen flex-col items-center">
+
+            <div>{ // throws crazy errors but its a working example of pulling from the server at least
+                dataResponse.map((items) => <div> {items.msg} </div>)
+            }</div>
             <p>COMFIRM INFORMATION</p>
 
             <div className='tblFoot'>
