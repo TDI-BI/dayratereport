91793fe554fbd40a92837cea71f254a08f13529e
eygwa <153556337+eygwa@users.noreply.github.com>
2024-04-29 12:28:52 -0500

    format changes
    report has been made into a componnent for ease of use, just want a last second backup before i paste in code from the internet that should give me pdf downloads

--- app/timesheet/review/page.tsx
+++ app/timesheet/review/page.tsx
@@ -1,47 +1,13 @@
 "use client"; // needed for interactivity
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { getPeriod } from '@/utils/payperiod';
+import Report from "@/components/report"
 
 export default function Page() {
-    let period = getPeriod();
-    const [dataResponse, setdataResponse] = useState([]);
-    useEffect(() => {
-      async function getPeriodInf(){
-        const apiUrlEndpoint = 'http://localhost:3000/api/getperiodinf';
-        const response = await fetch(apiUrlEndpoint);
-        const res = await response.json();
-        setdataResponse(res.resp);  
-      }
-      getPeriodInf();
-    }, []);
-
-    // really gotta think of a better way to do this tbh
-    let name=''
-    let daysworked=0
-    var dict: {[id: string] : string} = {};
-    dataResponse.forEach((item) => { // should build our dictionary mybe
-        name=item.uid;
-        if(item.ship) daysworked+=1;
-        dict[item.day]=item.ship
-    }) 
+    
 
     return (
         <main className="flex min-h-screen flex-col items-center">
-            <p><strong>COMFIRM REPORT</strong></p>
-            {/* LETS WORK ON REPORT FORMATTING*/}
-            <p> PERIOD REPORT FOR: {name}</p>
-            <div className='div2print'>{
-                period.map((day) => 
-                <div>
-                    <div className="reportLine" key={day}> 
-                        <p className='reportTxt'>{day}</p> : <p className='reportTxt'>{dict[day] ? dict[day] : ''}</p> 
-                    </div>
-                </div>) // for now we are jtus gonna try to pull 1 line
-                
-            }</div>
-            <p> TOTAL DAYS: {daysworked}</p>
-            {/* END SAMPLE REPORT */}
+            <Report/>
             <div className='tblFoot'>
                 <Link href='../'><div className='tblFootBtn'> confirm and submit </div></Link>
                 <Link href='../'><div className='tblFootBtn'> back </div></Link>
