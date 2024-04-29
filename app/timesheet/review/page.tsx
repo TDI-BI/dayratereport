ababc601fc5404b73b3f7e5ea2721f912f75f380
eygwa <153556337+eygwa@users.noreply.github.com>
2024-04-29 13:19:59 -0500

    canvas installed
    jspdf was working so now im going to try and get canvas hooked up

--- app/timesheet/review/page.tsx
+++ app/timesheet/review/page.tsx
@@ -1,15 +1,27 @@
 "use client"; // needed for interactivity
 import Link from "next/link";
 import Report from "@/components/report"
+import { jsPDF } from "jspdf";
 
 export default function Page() {
+
+    function bweh(){
+        doc.save("a4.pdf");
+    }
+
+    // Default export is a4 paper, portrait, using millimeters for units
+    //test
+    const doc = new jsPDF();
+
+    doc.text("Hello world!", 10, 10);
+    
     
 
     return (
         <main className="flex min-h-screen flex-col items-center">
             <Report/>
             <div className='tblFoot'>
-                <Link href='../'><div className='tblFootBtn'> confirm and submit </div></Link>
+                <button onClick={bweh}><div className='tblFootBtn'> confirm and submit </div></button>
                 <Link href='../'><div className='tblFootBtn'> back </div></Link>
             </div>
         </main>
