6b75853936aa83016bd8d926bf8c770d14a6c322
3 <153556337+eygwa@users.noreply.github.com>
2024-07-09 13:26:04 -0700

    change for local

--- utils/fetchBoth.tsx
+++ utils/fetchBoth.tsx
@@ -2,7 +2,7 @@ export const  fetchBoth = async (link:string) =>{
     let response;
     //function tries to fetch on both http and https to follow whatever ur on
     try {
-        response = await fetch('https://'+link); // remember to put the s back on
+        response = await fetch('http://'+link); // remember to put the s back on
     }
     catch (e){
         response = await fetch('https://www.'+link);
