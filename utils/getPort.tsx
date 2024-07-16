1ea8aec466f310b59d64904501a996e25dc177bc
eygwa <parkerseeley@tdi-bi.com>
2024-07-16 10:04:57 -0500

    fixed random port issues

--- utils/getPort.tsx
+++ utils/getPort.tsx
@@ -1,6 +1,6 @@
 export const getPort = () => {
     //if (process.env.NODE_ENV==='development') return 'localhost:3000'
-    //return 'tdifielddays.com'
-    return 'localhost:3000'
+    return 'tdifielddays.com'
+    //return 'localhost:3000'
 }
 
