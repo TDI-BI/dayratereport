420249453ea6b622fd13c21860c754d77e160909
3 <153556337+eygwa@users.noreply.github.com>
2024-07-26 09:58:43 -0700

    big changes to admin portal
    asiudfjaosidfjasouidf

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -33,6 +33,9 @@ const AdminPannel = () =>{
     const [dataResponse, setdataResponse] = useState([]);
     const [json, setjson] = useState(jtype);
 
+    const [crewEh, setCrewEh] = useState('all');
+    const [weeks, setWeeks] = useState('1');
+
     const period = getPeriod(periodEh);
 
     //database queries
@@ -106,19 +109,46 @@ const AdminPannel = () =>{
     //export csv
     const exportCsv = () =>{
         //setup label columns
-        const expTableData =    [{
-            cre:   'CREW',
-            fna:    "first name",
-            lna:    "last name",
-            mon:    period[0],
-            tue:    period[1],
-            wed:    period[2],
-            thu:    period[3],
-            fri:    period[4],
-            sat:    period[5],
-            sun:    period[6],
-            sum:    '',
-        }].concat(tblData)
+        const expTableData:any[] = [];
+
+        let tday:{[id: string] : string} = {};
+        tday['cre']='crew'
+        tday['fna']='first name'
+        tday['lna']='last name'
+        tday['sum']='total'
+
+        for(var i=Number(weeks)-1; i>=0; i--){
+            const nperiod = getPeriod(i+periodEh)
+            let day:{[id: string] : string} = {};
+            nperiod.map((p)=>{
+                day[p] = p
+            })
+
+            tday = {...tday, ...day}
+        }
+
+        Object.keys(json[shipEh]).map((name)=>{
+            let usr:{[id: string] : string} = {};
+            let sum=0;
+            for(const [id, value] of Object.entries(tday)){
+                
+                if(json[shipEh][name][id]){
+                    sum+=1;
+                    usr[id] = json[shipEh][name][id];
+                }
+                else usr[id]='';
+            }
+            usr['cre']=crewDict[userDict[name]];
+            usr['lna']=name.split('/')[0];
+            usr['fna']=name.split('/')[1];
+            usr['sum']=String(sum);
+            
+            if(sum!=0 && (usr['cre']==crewEh || crewEh=='all')){
+                expTableData.push(usr)
+                console.log(usr)
+            } 
+        })
+        console.log(expTableData)
 
         // generate from CSV, basically just copied from export-to-csv documentation
         const csvConfig = mkConfig({ 
@@ -132,42 +162,50 @@ const AdminPannel = () =>{
     return(
         <main className="flex min-h-screen flex-col items-center">
             <p>
-                <button className='tblFootBtn' onClick={()=>{setPeriodEh(periodEh+1)}}> {'< '} back</button>
+                <button className='tblFootBtn' key='back' onClick={()=>{setPeriodEh(periodEh+1)}}> {'< '} back</button>
                 {period[0]} to {period[6]}
-                <button className='tblFootBtn' onClick={()=>{setPeriodEh(periodEh-1)}}>forward {' >'}</button>
+                <button className='tblFootBtn' key='forward' onClick={()=>{setPeriodEh(periodEh-1)}}>forward {' >'}</button>
             </p>
             <div className='adminWrap'>
                 <div className='adminFilterWrap'>
                     <RadioGroup
+                        key='filter'
                         label='filter: '
                         value={shipEh}
                         onValueChange={(v)=>setShipEh(v)}
                     >
                         <Radio
+                            key='BMCC'
                             value='BMCC'
                             className={shipEh=='BMCC' ? 'uadminRB' : 'sadminRB'}
                         >BMCC</Radio>
                         <Radio
+                            key='EMMA'
                             value='EMMA'
                             className={shipEh=='EMMA' ? 'uadminRB' : 'sadminRB'}
                         >EMMA</Radio>
                         <Radio
+                            key='PROT'
                             value='PROT'
                             className={shipEh=='PROT' ? 'uadminRB' : 'sadminRB'}
                         >PROT</Radio>
                         <Radio
+                            key='GYRE'
                             value='GYRE'
                             className={shipEh=='GYRE' ? 'uadminRB' : 'sadminRB'}
                         >GYRE</Radio>
                         <Radio
+                            key='NAUT'
                             value='NAUT'
                             className={shipEh=='NAUT' ? 'uadminRB' : 'sadminRB'}
                         >NAUT</Radio>
                         <Radio
+                            key='3RD'
                             value='3RD'
                             className={shipEh=='3RD' ? 'uadminRB' : 'sadminRB'}
                         >3RD</Radio>
                         <Radio
+                            key='????'
                             value='????'
                             className={shipEh=='????' ? 'uadminRB' : 'sadminRB'}
                         >????</Radio>
@@ -175,8 +213,8 @@ const AdminPannel = () =>{
                 </div>
                 <div className='adminTable'>
                     <div className='adminRowLabel' key='headrow'>
-                        <div className='adminLabelX' key='headnamelbl'><strong>NAME</strong></div>
-                        <div className='adminLabelY' key='headnamelbl'>CREW</div>
+                        <div className='adminLabelX' key='headnamelblx'><strong>NAME</strong></div>
+                        <div className='adminLabelY' key='headnamelbly'>CREW</div>
                         {period.map((day)=>
                             <div className='adminLabelY' key={day+'label'}>
                                 <p>{day}</p>
@@ -198,7 +236,59 @@ const AdminPannel = () =>{
                     )}
                 </div>
             </div>
-            <button className='tblFootBtn' onClick={exportCsv}>export</button> 
+            <div className='tblHead'>
+                <RadioGroup
+                    className='exportSettings'
+                    key='weeks'
+                    label='Weeks: '
+                    value={weeks}
+                    onValueChange={(v)=>setWeeks(v)}        
+                >
+                    <Radio
+                        key='1'
+                        value='1'
+                        className={weeks=='1' ? 'uexportSetting' : 'sexportSetting'}
+                    >1</Radio>
+                    <Radio
+                        key='2'
+                        value='2'
+                        className={weeks=='2' ? 'uexportSetting' : 'sexportSetting'}
+                    >2</Radio>
+                    <Radio
+                        key='4'
+                        value='4'
+                        className={weeks=='4' ? 'uexportSetting' : 'sexportSetting'}
+                    >4</Radio>
+                </RadioGroup>
+
+
+                <RadioGroup
+                    className='exportSettings'
+                    key='crews'
+                    label='Crews: '
+                    value={crewEh}
+                    onValueChange={(v)=>setCrewEh(v)}        
+                >
+                    <Radio
+                        key='all'
+                        value='all'
+                        className={crewEh=='all' ? 'uexportSetting' : 'sexportSetting'}
+                    >all</Radio>
+                    <Radio
+                        key='domestic'
+                        value='domestic'
+                        className={crewEh=='domestic' ? 'uexportSetting' : 'sexportSetting'}
+                    >domestic</Radio>
+                    <Radio
+                        key='foreign'
+                        value='foreign'
+                        className={crewEh=='foreign' ? 'uexportSetting' : 'sexportSetting'}
+                    >foreign</Radio>
+                </RadioGroup>
+
+                <button className='tblFootBtn' onClick={exportCsv}>export</button> 
+            </div>
+
         </main>
     )
     
