8a4a8c111d13995f1d4a4e4eccd575d83e27d49e
eygwa <parkerseeley@tdi-bi.com>
2024-07-30 14:20:02 -0500

    idk changes i lost track. rewrote info page

--- app/daysworked/review/thanks/page.tsx
+++ app/daysworked/review/thanks/page.tsx
@@ -12,10 +12,10 @@ const profile = async () => {
         <main className="flex min-h-screen flex-col items-center"> 
                 <p> <strong> THANK YOU {name[0] + ' ' + name[1]}</strong> </p>
                 <p> your time sheet has been submitted! </p>
-                <p> please do not close the website until your field days worked report</p>
-                <p> until that has been downloaded. this will happen automatically. </p>
+                <p> a copy of your report should appear in your inbox within a few minutes. </p>
                 <p> you are only required to submit one report per pay period </p>
                 <p> you may resubmit if you find any issues within your report </p>
+                <p> issues/inquiries email parkerseeley@tdi-bi.com </p>
                 <Link href='../../../'><div className='tblFootBtn'>home</div></Link>
         </main>
     ) 
