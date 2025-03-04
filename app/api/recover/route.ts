a46cada2f546e2fe2f38e33b573007b4a4251b36
pSeeley <ava.irlol@gmail.com>
2025-03-04 14:08:13 -0600

    great improvments to recovery service

--- app/api/recover/route.ts
+++ app/api/recover/route.ts
@@ -8,6 +8,9 @@ import { getSession } from "@/actions";
 import { NextRequest } from "next/server";
 import { connectToDb } from "@/utils/connectToDb";
 
+
+
+
 export const GET = async (request: NextRequest) => {
     //block if logged in
     const session = await getSession();
@@ -33,9 +36,34 @@ export const GET = async (request: NextRequest) => {
         if (JSON.stringify(results) === "[]") throw { error: "no accnt found" };
         const resp = JSON.parse(JSON.stringify(results));
 
+
         //send email
         resp.map(async (e: any) => {
             // should only ever give one result
+
+            const htmlout = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>recovery emails</title>
+    </head>
+    <body style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; color: #333;">
+        <div style="background-color: #ffffff; padding: 20px; border-radius: 5px;">
+            <p style="font-size: 16px;">your recovery link can be accessed below</p>
+            <p style="font-size: 16px;">this link is one time use, do not allow others to see this email</p>
+    
+            <a href="${process.env.NEXT_PUBLIC_TYPE}${por}/login/resetpassword?acc=${e.password}" style="color: #0066cc; text-decoration: none;">
+                <div style="background-color: #0066cc; padding: 20px; border-radius: 10px; text-align: center; color:white">
+                    <p style="font-size: 16px; font-weight:600;">Recover Account</p>
+                </div>
+            </a>    
+    </div>
+</body>
+</html>
+`
+
             const data = await resend.emails.send({
                 from: "recover@tdifielddays.com", // we will change this probably
                 to: e.email, //e.email,
@@ -49,6 +77,7 @@ export const GET = async (request: NextRequest) => {
                     "/login/resetpassword?acc=" +
                     e.password +
                     "\ndo not allow others to see/use this link. It can only be used once. do not reply to this email",
+                html:htmlout,
             });
         });
 
